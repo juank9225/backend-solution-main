bbdb24f636e732dacfa8d3d19446ab68ae999664
Juan carlos salcedo <juank9225@gmail.com>
2021-07-07 16:26:48 -0500

    Se crea el test para el caso de uso crear question

--- src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
@@ -1,7 +1,47 @@
 package co.com.sofka.questions.usecases;
 
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import co.com.sofka.questions.usecase.CreateUseCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.Mockito.when;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CreateUseCaseTest {
+
+    @SpyBean
+    private CreateUseCase createUseCase;
+
+    @MockBean
+    private QuestionRepository questionRepository;
+
+    @Test
+    public void createQuestion(){
+
+        var questionDTO = new QuestionDTO("12","1","que fue primero", "OPEN","xxx");
+        var question= new Question();
+        question.setId("12");
+        question.setQuestion("que fue primero");
+        question.setUserId("1");
+        question.setType("OPEN");
+        question.setCategory("xxx");
+
+        when(questionRepository.save(Mockito.any(Question.class))).thenReturn(Mono.just(question));
+
+        var respuesta = createUseCase.insertar(questionDTO);
+
+        Assertions.assertEquals(respuesta.block().getId(),"12");
+
+    }
 
 }
